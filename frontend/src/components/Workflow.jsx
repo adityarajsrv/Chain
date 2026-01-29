68ba1949f9ce0b4970950cc8c6cdf68c5da2eeeb
Aditya <adityarajsrvofficial@gmail.com>
2026-01-30 00:23:51 +0530

    Created workflow background

--- frontend/src/components/Workflow.jsx
+++ frontend/src/components/Workflow.jsx
@@ -1,6 +1,6 @@
 const Workflow = () => {
   return (
-    <main className="ml-64 px-8 min-h-screen text-white">
+    <main className="ml-56 px-8 min-h-screen overflow-auto bg-[#fafafa] bg-[radial-gradient(circle,rgba(0,0,0,0.08)_1px,transparent_1px)] bg-size-[24px_24px]">
 
     </main>
   )
